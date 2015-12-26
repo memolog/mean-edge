47b1d502237ea92032f77a1141706b7b553b11d5
Yutaka Yamaguchi <yyamaguchi@sixapart.com>
2015-12-27 08:58:20 +0900

    Move node_modules up to root directory

--- webpack/frontend.webpack.config.js
+++ webpack/frontend.webpack.config.js
@@ -27,10 +27,10 @@ module.exports = {
         query: {
           // copied from https://github.com/AngularClass/angular2-webpack-starter/blob/master/webpack.config.js
           'ignoreDiagnostics': [
-            2403, // 2403 -> Subsequent variable declarations
-            2300, // 2300 -> Duplicate identifier
-            2374, // 2374 -> Duplicate number index signature
-            2375  // 2375 -> Duplicate string index signature
+            // 2403, // 2403 -> Subsequent variable declarations
+            // 2300, // 2300 -> Duplicate identifier
+            // 2374, // 2374 -> Duplicate number index signature
+            // 2375  // 2375 -> Duplicate string index signature
           ]
         },
       }
